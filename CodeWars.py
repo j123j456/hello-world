4176a81ab110dd64b3854ebc3332445aa79cd026
Jamie Tian <42948354+j123j456@users.noreply.github.com>
2018-09-06 16:59:14 -0400

    Create CodeWars.py

---
+++ CodeWars.py
@@ -0,0 +1,11 @@
+//Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+
+def sum_dig_pow(a, b): # range(a, b + 1) will be studied by the function
+        eur = []
+    for n in range(a,b+1):
+        for i in range(len(str(n))
+        
+        if sumpow == n:
+            eur.append(n)
+    
+    return eur
